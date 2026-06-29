807cc2ab09648815edd33f75b8252f6ea381207a
Sam Wilson <66247620+swizzle42@users.noreply.github.com>
2026-06-29 14:44:29 +0000

    initial server socket

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,25 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
 int main() {
+    // create a socket
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    // print the socket FD
+    std::cout << "server socket FD: " << serverSocket << std::endl;
+
+    // specify the address
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8080);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+    // bind the socket
+    bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+
+    // listen to the assigned socket
+    listen(serverSocket, 5);
+
     return 0;
 }
