8472919c1d9a128f00899a9c82da673dd6345d79
Sam Wilson <66247620+swizzle42@users.noreply.github.com>
2026-06-29 15:03:33 +0000

    prepare for client connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,37 @@
 #include <iostream>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <unistd.h>
 
 int main() {
     // create a socket
-    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
     // print the socket FD
-    std::cout << "server socket FD: " << serverSocket << std::endl;
+    std::cout << "server socket FD: " << server_socket << std::endl;
 
     // specify the address
-    sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(8080);
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(8080);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
 
     // bind the socket
-    bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+    bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr));
 
     // listen to the assigned socket
-    listen(serverSocket, 5);
+    listen(server_socket, 5);
+
+    // accept an incoming connection (blocking)
+    sockaddr_in client_addr;
+    socklen_t client_len = sizeof(client_addr);
+
+    std::cout << "Waiting for inbound connection" << std::endl;
+    int client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_len);
+    std::cout << "Client connected. Assigned FD: " << client_socket << std::endl;
+
+    close(client_socket);
+    close(server_socket);
 
     return 0;
 }
