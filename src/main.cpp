a4900ea2da6139b0ea42e87fc858e9e4d3d01ab7
Sam Wilson <66247620+swizzle42@users.noreply.github.com>
2026-06-29 15:36:22 +0000

    refactor project structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,6 @@
-#include <iostream>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
+#include "controls_middleware/server.h"
 
 int main() {
-    // create a socket
-    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
-
-    // print the socket FD
-    std::cout << "server socket FD: " << server_socket << std::endl;
-
-    // specify the address
-    sockaddr_in server_addr;
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(8080);
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-
-    // bind the socket
-    bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr));
-
-    // listen to the assigned socket
-    listen(server_socket, 5);
-
-    // accept an incoming connection (blocking)
-    sockaddr_in client_addr;
-    socklen_t client_len = sizeof(client_addr);
-
-    std::cout << "Waiting for inbound connection" << std::endl;
-    int client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_len);
-    std::cout << "Client connected. Assigned FD: " << client_socket << std::endl;
-
-    close(client_socket);
-    close(server_socket);
-
+    server();
     return 0;
 }
